6f973593bfa552a88a593141bfd17a7dc98864ca
Janitha133 <33251135+Janitha133@users.noreply.github.com>
2020-10-05 21:35:12 +0530

    Create calculateMedianInArray.py
    Create Python code To Calculate Median

---
+++ Python/calculateMedianInArray.py
@@ -0,0 +1,13 @@
+# Python code to demonstrate the working of median() function. 
+
+# importing statistics module 
+import statistics 
+
+# unsorted list of random integers 
+data1 = [2, -2, 3, 6, 9, 4, 5, -1] 
+
+
+# Printing median of the 
+# random data-set 
+print("Median is : % s "
+		% (statistics.median(data1)))
